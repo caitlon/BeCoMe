40a0840bf21ff7741481dae4bda797644fb0c436
Katya <caiton@pm.me>
2025-10-06 21:07:29 +0200

    feat: add BeCoMeCalculator class skeleton

---
+++ src/calculators/__init__.py
@@ -0,0 +1,11 @@
+"""
+Calculators for the BeCoMe method.
+
+This module contains calculators for computing the Best Compromise Mean
+from expert opinions represented as fuzzy triangular numbers.
+"""
+
+from .become_calculator import BeCoMeCalculator
+
+__all__ = ["BeCoMeCalculator"]
+
