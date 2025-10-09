abf32325a26ef559d80fd5d3e5ea4bd6c94059dd
Katya <caiton@pm.me>
2025-10-09 19:17:52 +0200

    refactor: rename data/ to reference/

--- tests/reference/__init__.py
+++ tests/reference/__init__.py
@@ -1,5 +1,5 @@
 """
-Test data module containing Excel reference cases.
+Reference test cases module from Excel implementation.
 
 This module exports all case studies from the Excel reference implementation:
 - BUDGET_CASE: 22 experts (even), COVID-19 budget support
@@ -7,8 +7,8 @@ This module exports all case studies from the Excel reference implementation:
 - FLOODS_CASE: 13 experts (odd), flood prevention measures
 """
 
-from tests.data.budget_case import BUDGET_CASE
-from tests.data.floods_case import FLOODS_CASE
-from tests.data.pendlers_case import PENDLERS_CASE
+from tests.reference.budget_case import BUDGET_CASE
+from tests.reference.floods_case import FLOODS_CASE
+from tests.reference.pendlers_case import PENDLERS_CASE
 
 __all__ = ["BUDGET_CASE", "FLOODS_CASE", "PENDLERS_CASE"]
