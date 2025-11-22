f4c37ba179b9504606315678a15d0e65e87641dc
Katya <caiton@pm.me>
2025-11-22 09:40:10 +0100

    refactor: unify init imports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,1 @@
 """BeCoMe data models."""
-
-from .become_result import BeCoMeResult
-from .expert_opinion import ExpertOpinion
-from .fuzzy_number import FuzzyTriangleNumber
-
-__all__ = ["BeCoMeResult", "ExpertOpinion", "FuzzyTriangleNumber"]
